66c77dd7baee3847d90480f7dc925178decb6319
shaopengfei <shaopengfei@xiaomi.com>
2016-10-26 10:31:57 +0800

    router in js

--- src/static/entry/index.js
+++ src/static/entry/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router} from 'react-router';
+import { Router,Route,hashHistory} from 'react-router';
 import createBrowserHistory from "history/lib/createBrowserHistory";
 import routes from '../config/routes';
+
 class Index extends React.Component {
   constructor(props) {
     super(props);
--- src/static/page1/page1.jsx
+++ src/static/page1/page1.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Link} from 'react-router';
 
-class Page1 extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
+var Page1  = React.createClass({
+  // ask for `router` from context
+  contextTypes: {
+    router: React.PropTypes.isRequired
+  },
+  onhandle:function(){
+    // browserHistory.push('/page2');
+    this.props.history.push('/page2');
+    // this.context.router.push('/page2')
+  },
+  render:function() {
     return (
       <div>
-      	<span>1111</span>
+        <span onClick={this.onhandle}>1111</span>
         <Link to="/page2">下一页</Link>
       </div>
     )
   }
-}
+})
 module.exports = Page1 ;

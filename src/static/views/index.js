86acde24f60f1179c6e1df26bc16af0aed8a8027
shaopengfei <shaopengfei@xiaomi.com>
2016-11-05 15:30:19 +0800

    dev

--- src/static/views/index.js
+++ src/static/views/index.js
@@ -11,7 +11,29 @@ var Index  = React.createClass({
     return (
       <div className="index-content">
          <div className="index-banner">
-            <div className="banner-content"></div>
+            <div className="banner-content">
+              <p className="banner-tit">拉勾企业服务·人才招聘</p>
+              <ul>
+                <li>行业领先的垂直互联网招聘平台</li>
+                <li>1081万+的候选人才库</li>
+                <li>每月简历投递量472万+</li>
+              </ul>
+              <button className="view-btn">查看招聘人才产品以及解决方案</button>
+            </div>
+         </div>
+         <div className="index-tab">
+            <ul>
+              <li className="zhaopin off">
+                <i></i>
+                <span>人才招聘</span>
+              </li>
+              <li className="jianshe off">
+                <span>品牌建设</span>
+              </li>
+              <li className="guanli off">
+                <span>人力资源管理</span>
+              </li>
+            </ul>
          </div>
       </div>
     )

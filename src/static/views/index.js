0649db72bb8175154f3af7498968a8f46d84f9cf
shaopengfei <shaopengfei@xiaomi.com>
2016-11-05 16:11:50 +0800

    dev

--- src/static/views/index.js
+++ src/static/views/index.js
@@ -23,14 +23,16 @@ var Index  = React.createClass({
          </div>
          <div className="index-tab">
             <ul>
-              <li className="zhaopin off">
+              <li className="zhaopin on">
                 <i></i>
                 <span>人才招聘</span>
               </li>
               <li className="jianshe off">
+                <i></i>
                 <span>品牌建设</span>
               </li>
               <li className="guanli off">
+                <i></i>
                 <span>人力资源管理</span>
               </li>
             </ul>

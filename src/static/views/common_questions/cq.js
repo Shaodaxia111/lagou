e71db8901762f4c6bcec1bbdd6852bbdc6dd422e
刘迪 <liudi@rd.tuan800.com>
2016-11-06 10:05:05 +0800

    常见问题 push test

---
+++ src/static/views/common_questions/cq.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+require("./cq.css");
+
+var Index = React.createClass({
+    // onhandle:function(){
+    //   // browserHistory.push('/page2');
+    //   this.props.history.push('/page2');
+    //   // this.context.router.push('/page2')
+    // },
+    render: function() {
+        return ( 
+            < div className = "index-content" >
+                'ddddd'
+            < /div>
+        )
+    }
+})
+module.exports = Index;

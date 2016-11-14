aff6566cd5f5404711920858e4d63bd6beeab828
shaopengfei <shaopengfei@xiaomi.com>
2016-11-14 20:51:39 +0800

    dev

--- src/static/views/success/success.js
+++ src/static/views/success/success.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import IndexTab from '../../component/indexTab';
 require("./success.css");
+var bannerBg = commonPath + "img/success_banner.jpg";
 var Success  = React.createClass({
+  getInitialState:function(){
+      return {
+        bannerHeight:parseInt(document.body.clientWidth*180/1920)
+      }
+  },
   render:function() {
     return (
       <div className="success-content">
         <div className="success-banner">
-          <div className="banner-content">
+          <img src={bannerBg} alt = '立即获得专属顾问一对一服务'/>
+          <div style={{lineHeight:this.state.bannerHeight+"px"}} className="content">
             <p className="success-tit">立即获得专属顾问一对一服务</p>
           </div>
         </div>
@@ -16,7 +23,9 @@ var Success  = React.createClass({
             <p className="c-tit">您的联系方式我们已收到，专属顾问会在三个工作日内联系您。</p>
             <p className="c-tit">了解更多，欢迎查看下方更多解决方案</p>
         </div>
-        <IndexTab />
+        <div className="success-tab">
+           <IndexTab />
+        </div>
       </div>
     )
   }
